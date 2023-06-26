57d0cf1e0aaa00451ce2aaa7ad4d39d5430c0a5d
alantiren <tirenalan0@gmail.com>
2023-06-26 17:37:35 +0300

    updates

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
  * swap_ints - Swap two integers in an array.
@@ -9,7 +8,6 @@
 void swap_ints(int *a, int *b)
 {
 int tmp;
-
 tmp = *a;
 *a = *b;
 *b = tmp;
@@ -24,13 +22,10 @@ tmp = *a;
 void shell_sort(int *array, size_t size)
 {
 size_t gap, i, j;
-
 if (array == NULL || size < 2)
 return;
-
 for (gap = 1; gap < (size / 3);)
 gap = gap * 3 + 1;
-
 for (; gap >= 1; gap /= 3)
 {
 for (i = gap; i < size; i++)
